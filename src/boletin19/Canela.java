78f91369e744f6b74235e6f4ef825a887e8134b9
alejandro <alejangomez27@gmail.com>
2017-03-12 18:03:38 +0100

    Ramita de canela: el metodo

---
+++ src/boletin19/Canela.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin19;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Canela {
+    
+    public void ramitaDeCanela(){
+        
+        System.out.println("Esto es una ramita de canela (2).");
+    }
+    
+}
